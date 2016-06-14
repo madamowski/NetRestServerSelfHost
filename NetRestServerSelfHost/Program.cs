c8904c46902770ecb9029ae7870e7f86fd793a17
Marcin Adamowski <marcin.adamowski@gmail.com>
2016-06-14 23:16:02 +0100

    Initial test

---
+++ NetRestServerSelfHost/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestServerSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quick File Server
+            //var url = "http://localhost:8080";
+            //WebApp.Start(url, builder => builder.UseFileServer(enableDirectoryBrowsing: true));
+            //Console.WriteLine("Listening at " + url);
+            //Console.ReadLine();
+
+            //return;
+
+
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                System.Console.WriteLine("Server running on port: " + baseAddress);
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.ReadLine();
+            }            
+        }
+    }
+}
---
+++ NetRestServerSelfHost/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestServerSelfHost
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            //Enable directory browsing
+            //appBuilder.UseFileServer(enableDirectoryBrowsing: true);
+
+            appBuilder.UseFileServer(new FileServerOptions()
+            {
+                RequestPath = new PathString(""),
+                FileSystem = new PhysicalFileSystem(@".\Views"),
+            });
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
